7c047e2086fa85bfb9808db618d12188b4d17ed2
alekhya2009 <agarimel@gitam.in>
2025-01-10 10:22:33 +0530

    Update question1.c

--- question1.c
+++ question1.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    int arr[] = {1, 2, 3, 4, 3, 5}; // Example array
-    int key = 3;                    // Key to remove
-    int n = sizeof(arr) / sizeof(arr[0]);
+int countNotEqual(int arr[], int size, int key) {
     int count = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i] != key) {
-            count++;
-        }
+    for (int i = 0; i < size; i++) {
+        if (arr[i] != key) count++;
     }
+    return count;
+}
 
-    printf("Number of values not equal to %d: %d\n", key, count);
+int main() {
+    int arr[] = {1, 2, 3, 2, 4};
+    int key = 2;
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printf("Count of elements not equal to %d: %d\n", key, countNotEqual(arr, size, key));
     return 0;
 }
