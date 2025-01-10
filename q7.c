040fd952b6cf3662dfe1e0c99c0080ce74fb3070
alekhya2009 <agarimel@gitam.in>
2025-01-10 10:25:56 +0530

    Create q7.c

---
+++ q7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int isSubstring(char *str1, char *str2) {
+    return strstr(str1, str2) != NULL;
+}
+
+int main() {
+    char str1[] = "Hello, world!";
+    char str2[] = "world";
+    if (isSubstring(str1, str2)) {
+        printf("'%s' is a substring of '%s'\n", str2, str1);
+    } else {
+        printf("'%s' is not a substring of '%s'\n", str2, str1);
+    }
+    return 0;
